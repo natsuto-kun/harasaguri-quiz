bd1ef142da197fa7f91a2028f0c4b9f35e501165
natsuto-kun <natsuto391@gmail.com>
2022-07-11 00:45:57 +0900

    add: emotion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type {AppProp} from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({Component, pageProps}: AppProps) => (
+  <>
+    <Head>
+      <meta name="viewport"/>
+    </Head>
+    <Component {...pageProps}/>
+  </>
+);
 
-export default MyApp
+export default MyApp;
