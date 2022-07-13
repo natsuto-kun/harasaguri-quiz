5cc0f055793313511a7e7f940f18e95db395b442
natsuto-kun <natsuto391@gmail.com>
2022-07-13 09:32:12 +0900

    add: utils

---
+++ utils/game-text.ts
@@ -0,0 +1,15 @@
+type GameText = {
+    id: number
+    text: string
+  }
+
+export const GameTextData: GameText[] = [
+  {
+    id: 1,
+    text: 'さんぷるだよ〜',
+  },
+  {
+    id: 2,
+    text: 'クイズのサンプルだよ〜',
+  },
+];
