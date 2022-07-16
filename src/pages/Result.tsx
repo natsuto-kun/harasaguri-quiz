bbd20d5e2b6b0b17785a6974d5f9cb05a006dc29
kiseki-kiseki <kcanggua@gmail.com>
2022-07-16 14:35:05 +0900

    LocalStoraged導入

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ResultPage = () => {
+  const [countAnswer, setCountAnswer] = useState(0);
+  const getLocalStorage = async (key: string): Promise<string> => {
+    const a = await localStorage.getItem(key);
+    if (a !== null) {
+      return a;
+    } else {
+      return '';
+    }
+  };
+  useEffect(() => {
+    const getLocalStorageData = async () => {
+      const getCountAnswer = await getLocalStorage('countAnswer');
+      setCountAnswer(JSON.parse(getCountAnswer));
+    };
+    getLocalStorageData();
+  }, []);
   return (
     <div>
+      {countAnswer}
       <h2>リザルトになる予定</h2>
     </div>
   );
