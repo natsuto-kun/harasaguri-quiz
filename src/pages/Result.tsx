dd11533f7663d6ffdf76fc4feec6da93d5c4c8d3
ysk-rl <oyskrin0@outlook.jp>
2022-07-17 17:05:02 +0900

    add: twitter button

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable new-cap */
+import Script from 'next/script';
 import React, { useState, useEffect } from 'react';
 
 const ResultPage = () => {
@@ -20,9 +21,17 @@ const ResultPage = () => {
     getLocalStorageData();
   }, []);
 
+  
+
+  const twitterText =  countAnswer + "やっはろー"
+
   return (
     <div>
       <h2>正解数: {countAnswer}</h2>
+      <div>
+        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-text={twitterText} data-show-count="false"></a>
+        <Script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></Script>
+      </div>
     </div>
   );
 };
