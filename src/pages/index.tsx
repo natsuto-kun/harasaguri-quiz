f66a340f7eb6ee290f6b3d02166c99fe1729933f
natsuto-kun <natsuto391@gmail.com>
2022-07-17 16:42:44 +0900

    add: slider-images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
-import type { NextPage } from 'next'
-import { css } from '@emotion/react'
-import Link from 'next/link'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-=======
 import type { NextPage } from 'next';
 import { css } from '@emotion/react';
 import Link from 'next/link';
->>>>>>> origin/develop
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Image from 'next/image';
 
 const SampleText = css`
   font-weight: bold;
@@ -20,6 +15,18 @@ const DeleteLink = css`
   display: none;
 `;
 
+const ImageContainer = css`
+  padding: 40px;
+`;
+
+const container = css`
+  margin-bottom: 40px;
+`;
+
+const start = css`
+  margin-top: 40px;
+`;
+
 const Home: NextPage = () => {
   const settings = {
     dots: true,
@@ -28,40 +35,38 @@ const Home: NextPage = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrow: true,
-    autoplay: false,
-  }
+    autoplay: true,
+  };
   const items = [
-    { id: 1, title: 'title1', img: './favicon.ico' },
-    { id: 2, title: 'title2', img: './favicon.ico' },
-    { id: 3, title: 'title3', img: './favicon.ico' },
-    { id: 4, title: 'title4', img: './favicon.ico' },
-    { id: 5, title: 'title5', img: './favicon.ico' },
-    { id: 6, title: 'title6', img: './favicon.ico' },
-  ]
+    { id: 1, title: 'はらさぐりにようこそ', img: '/images/welcome.png' },
+    { id: 2, title: '相手は回りくどい言葉を使ってくるよ', img: '/images/welcome2.png' },
+    { id: 3, title: '相手の本心を当てよう！', img: '/images/welcome3.png' },
+    { id: 4, title: '相手を怒らせないように気をつけよう', img: '/images/welcome4.png' },
+    { id: 5, title: '間違えすぎると...', img: '/favicon.ico' },
+  ];
   return (
-    <div>
+    <div css={container}>
       <p css={SampleText}>サンプルだよ〜</p>
-      <div>
-        <p>yahharo-</p>
-      </div>
       <div>
         <Slider {...settings}>
           {items.map((item) => {
             return (
-              <div key={item.id}>
+              <div css={ImageContainer} key={item.id}>
                 <p>{item.title}</p>
-                <img src={item.img} />
+                <Image width={400} height={400} src={item.img} />
               </div>
-            )
+            );
           })}
         </Slider>
       </div>
-      <Link href='/Quiz' passHref>
-        <a>Quiz Page</a>
-      </Link>
-      <Link href='/Result' passHref>
-        <a css={DeleteLink}>Result Page</a>
-      </Link>
+      <div css={start}>
+        <Link href='/Quiz' passHref>
+          <a>Quiz Page</a>
+        </Link>
+        <Link href='/Result' passHref>
+          <a css={DeleteLink}>Result Page</a>
+        </Link>
+      </div>
     </div>
   );
 };
