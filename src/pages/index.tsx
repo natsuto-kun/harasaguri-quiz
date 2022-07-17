503a3d61b9480afe13d7d029ce95c892854c7937
natsuto-kun <natsuto391@gmail.com>
2022-07-18 04:33:36 +0900

    add: メディアクエリの設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,11 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
+import { mq } from '../layouts/mq';
 
 const SampleText = css`
   font-weight: bold;
-  font-size: 28px;
+  font-size: 24px;
 `;
 
 const DeleteLink = css`
@@ -17,14 +18,84 @@ const DeleteLink = css`
 
 const ImageContainer = css`
   padding: 40px;
+  opacity: 0.75;
 `;
 
-const container = css`
-  margin-bottom: 40px;
+const TopContainer = css`
+  ${mq[2]}{
+    padding:0% 30%
+  }}
+`;
+
+const StartButton = css`
+  width: 100%;
+  height: 100%;
+  color: #18978f;
+  padding: 10px;
+  font-size: 20px;
 `;
 
 const start = css`
-  margin-top: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  height: 50px;
+  position: relative;
+  background: #F7ECDE;
+  border: none;
+  border-radius: 30px;
+  box-sizing: border-box;
+  padding: 0 45px 0 25px;
+  color: #E9DAC1;
+  font-size: 16px;
+  letter-spacing: 0.1em;
+  line-height: 1.3;
+  text-align: center;
+  text-decoration: none;
+  transition-duration: 0.3s;
+  margin: auto;
+  padding: 15px: 60px;
+  box-shadow: 0px 8px 10px -7px;
+  &:before{
+    content: '';
+    width: 8px;
+    height: 8px;
+    border: 0;
+    border-top: 2px solid #54BAB9;
+    border-right: 2px solid #54BAB9;
+    transform: rotate(45deg);
+    position: absolute;
+    top: 50%;
+    right: 25px;
+    margin-top: -6px;
+  }
+  &:hover{
+    background: #F7ECDE;
+    color: #54BAB9;
+    box-shadow: 0px 8px 10px -7px;
+  }
+  &:hover::before{
+      content: '';
+      width: 8px;
+      height: 8px;
+      border: 0;
+      border-top: 2px solid #54BAB9;
+      border-right: 2px solid #54BAB9;
+      transform: rotate(45deg);
+      position: absolute;
+      top: 50%;
+      right: 25px;
+      margin-top: -6px;
+  }
+`;
+
+const sliderWrapper = css`
+  background: #f7ecde;
+  border: none;
+  border-radius: 15px;
+  margin-bottom: 40px;
+  box-shadow: 0px 8px 10px -7px;
 `;
 
 const Home: NextPage = () => {
@@ -38,21 +109,20 @@ const Home: NextPage = () => {
     autoplay: true,
   };
   const items = [
-    { id: 1, title: 'はらさぐりにようこそ', img: '/images/welcome.png' },
+    { id: 1, title: 'はらさぐりくいずへようこそ', img: '/images/welcome.png' },
     { id: 2, title: '相手は回りくどい言葉を使ってくるよ', img: '/images/welcome2.png' },
     { id: 3, title: '相手の本心を当てよう！', img: '/images/welcome3.png' },
     { id: 4, title: '相手を怒らせないように気をつけよう', img: '/images/welcome4.png' },
     { id: 5, title: '間違えすぎると...', img: '/images/welcome5.png' },
   ];
   return (
-    <div css={container}>
-      <p css={SampleText}>サンプルだよ〜</p>
+    <div css={TopContainer}>
       <div>
-        <Slider {...settings}>
+        <Slider css={sliderWrapper} {...settings}>
           {items.map((item) => {
             return (
               <div css={ImageContainer} key={item.id}>
-                <p>{item.title}</p>
+                <p css={SampleText}>{item.title}</p>
                 <Image width={400} height={400} src={item.img} />
               </div>
             );
@@ -61,7 +131,7 @@ const Home: NextPage = () => {
       </div>
       <div css={start}>
         <Link href='/Quiz' passHref>
-          <a>Quiz Page</a>
+          <a css={StartButton}>Start!</a>
         </Link>
         <Link href='/Result' passHref>
           <a css={DeleteLink}>Result Page</a>
