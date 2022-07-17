00dce1ae296a3df115d520da80eb41645f5100d5
kiseki-kiseki <kcanggua@gmail.com>
2022-07-17 16:13:42 +0900

    実装完了♡

--- components/Random.ts
+++ components/Random.ts
@@ -1,9 +1,33 @@
 const RandomNum = (num: number) => {
-  const NumArray = [];
+  const NumArray: number[] = [];
   for (let i = 0; i < num; i++) {
-    NumArray.push(Math.floor(Math.random() * num));
+    while (true) {
+      const random = Math.floor(Math.random() * num);
+      if (!NumArray.includes(random)) {
+        NumArray.push(random);
+        break;
+      }
+    }
   }
   return NumArray;
 };
 
 export default RandomNum;
+
+// import React, { useState } from 'react';
+
+// const RandomNum = (num: number) => {
+//   const [NumArray, setNumArray] = useState<number[]>([]);
+//   for (let i = 0; i < num; i++) {
+//     while (true) {
+//       const random = Math.floor(Math.random() * num);
+//       if (!NumArray.includes(random)) {
+//         setNumArray((prev) => [...prev, random]);
+//         break;
+//       }
+//     }
+//   }
+//   return NumArray;
+// };
+
+// export default RandomNum;
