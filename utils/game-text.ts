2d0f66d30069d5d52c0408d4b09ca680522e220c
ysk-rl <oyskrin0@outlook.jp>
2022-07-18 10:20:14 +0900

    add : 問題文の追加

--- utils/game-text.ts
+++ utils/game-text.ts
@@ -1,7 +1,6 @@
 type QuizData = {
   id: number;
   question: string;
-  image: string;
   answers: string[];
   correct: string;
   selectedAnswer: string;
@@ -10,16 +9,14 @@ type QuizData = {
 export const QuizTextData: QuizData[] = [
   {
     id: 1,
-    question: '問題文を入れてね',
-    image: '画像のパスを入れてね',
-    answers: ['answer1', 'answer2', 'answer3', 'answer4'],
-    correct: '正解の選択肢を入れてね',
+    question: '独創的やね！',
+    answers: ['おかしいね', 'answer2', 'answer3', 'answer4'],
+    correct: 'おかしいね',
     selectedAnswer: '',
   },
   {
     id: 2,
     question: 'お茶漬けを出された！',
-    image: 'お茶漬け画像',
     answers: ['もっと居ていいよ', 'はよ帰ってや', '次に何か奢れよ', 'たんと食え '],
     correct: 'はよ帰ってや',
     selectedAnswer: '',
@@ -27,25 +24,100 @@ export const QuizTextData: QuizData[] = [
   {
     id: 3,
     question: '「いい腕時計してはりますなぁ」',
-    image: '腕時計画像',
     answers: ['金持ちやね', '話長すぎんで', '奢ってくれ', '思いつかね〜'],
     correct: '話長すぎんで',
     selectedAnswer: '',
   },
   {
     id: 4,
-    question: '問題文思いつかへん',
-    image: 'fuga',
-    answers: ['ぽぐ', 'POG', 'vaey POG', 'ぽおおおぐ'],
-    correct: 'POG',
+    question: '元気なお子さんですね',
+    answers: ['静かにさせろ', 'POG', 'vaey POG', 'ぽおおおぐ'],
+    correct: '静かにさせろ',
     selectedAnswer: '',
   },
   {
     id: 5,
-    question: '問題文募集中',
-    image: 'hoge',
-    answers: ['hei', 'guys', 'we', 'have'],
-    correct: 'have',
+    question: '（仕事で）マイペースだね',
+    answers: ['作業遅いね', 'guys', 'we', 'have'],
+    correct: '作業遅いね',
+    selectedAnswer: '',
+  },
+  {
+    id: 6,
+    question: 'きれいな柄ですね',
+    answers: ['派手過ぎませんか', 'うらやましい', '似合ってるね', 'have'],
+    correct: '派手すぎませんか',
+    selectedAnswer: '',
+  },
+  {
+    id: 7,
+    question: 'よう勉強してはりますな',
+    answers: ['hei', '余計な事言わないで', 'we', 'have'],
+    correct: '余計な事いわないで',
+    selectedAnswer: '',
+  },
+  {
+    id: 8,
+    question: 'いい性格してるね',
+    answers: ['hei', 'guys', 'we', '無神経だね'],
+    correct: '無神経だね',
+    selectedAnswer: '',
+  },
+  {
+    id: 9,
+    question: '最近忙しそうだね',
+    answers: ['hei', '会えなくて寂しい', 'we', 'have'],
+    correct: '会えなくて寂しい',
+    selectedAnswer: '',
+  },
+  {
+    id: 10,
+    question: '限界を知ってるんだね',
+    answers: ['hei', 'guys', '諦めが早いね', 'have'],
+    correct: '諦めが早いね',
+    selectedAnswer: '',
+  },
+  {
+    id: 11,
+    question: '基本に忠実だね',
+    answers: ['answer1', 'answer2', 'answer3', '応用がきかないね'],
+    correct: '応用がきかないね',
+    selectedAnswer: '',
+  },
+  {
+    id: 12,
+    question: '今仕事が忙しいんです',
+    answers: ['answer1', 'answer2', 'プライベートに踏み込まないで', 'answer4'],
+    correct: 'プライベートに踏み込まないで',
+    selectedAnswer: '',
+  },
+  {
+    id: 13,
+    question: '検討させていただきます',
+    answers: ['現実的じゃないです', 'answer', 'answer3', 'answer4'],
+    correct: '現実的じゃないです',
+    selectedAnswer: '',
+  },
+  {
+    id: 14,
+    question: '優しくていい人だと思う',
+    answers: ['answer1', 'answer2', 'answer3', '私にはどうでもいい人'],
+    correct: '正解の選択肢を入れてね',
+    selectedAnswer: '私にはどうでもいい人',
+  },
+  {
+    id: 15,
+    question: '（大勢の集まりで）しっかりしてるね！',
+    answers: ['answer1', 'answer2', '幹事よろしく', 'answer4'],
+    correct: '幹事よろしく',
+    selectedAnswer: '',
+  },
+  {
+    id: 16,
+    question: 'イケメンだね',
+    answers: ['answer1', 'answer2', 'answer3', 'イケメン風だね'],
+    correct: 'イケメン風だね',
     selectedAnswer: '',
   },
 ];
+
